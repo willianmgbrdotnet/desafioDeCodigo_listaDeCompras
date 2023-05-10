d56e15b8d4dd57c3cc803c97e8d56ac911763925
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-05-10 20:06:45 -0300

    Desafio Completado

--- Program.cs
+++ Program.cs
@@ -10,7 +10,18 @@ class ComprasNoSupermercado
   
     for (int i = 0; i < numeroDeTestes; i++) {
       List<string> listDeCompras = new List<String>(Console.ReadLine().Split(' '));
-    }
+
+      List<string> listaSemDuplicados = listDeCompras.Distinct().ToList();
+
+      listaSemDuplicados.Sort();
+
+      foreach(string item in listaSemDuplicados)
+      {
+        Console.Write($"{item} ");
+      }
+
+      System.Console.WriteLine();
     
+    }
   }
 }
