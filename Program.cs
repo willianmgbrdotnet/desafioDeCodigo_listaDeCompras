724b3c99c1614823b3c9ce5a5a8cb8c0ca5dbd04
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-05-10 19:33:26 -0300

    Codigo base do Desafio de Codigo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ComprasNoSupermercado
+{
+  public static void Main(string[] args)
+  {
+    int numeroDeTestes = int.Parse(Console.ReadLine());
+  
+    for (int i = 0; i < numeroDeTestes; i++) {
+      List<string> listDeCompras = new List<String>(Console.ReadLine().Split(' '));
+    }
+    
+  }
+}
